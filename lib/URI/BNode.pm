768f164748869580314abc93592af983e120ce8f
Dorian Taylor <dorian.taylor.lists@gmail.com>
2013-11-03 18:36:27 -0800

    woopsies

--- lib/URI/BNode.pm
+++ lib/URI/BNode.pm
@@ -38,11 +38,11 @@ URI::BNode - RDF blank node identifiers which are also URI objects
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
